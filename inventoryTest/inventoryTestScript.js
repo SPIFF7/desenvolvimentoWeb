090fa7e66b4b10940b8cf8092b2350061acfe5e7
Mário Abrantes <mabrante@student.dei.uc.pt>
2021-11-17 19:46:27 +0000

    Tabs added to inventory modal.

--- inventoryTest/inventoryTestScript.js
+++ inventoryTest/inventoryTestScript.js
@@ -15,3 +15,21 @@ window.onclick = function (event){
         inventoryModal.style.display = "none";
     }
 }
+
+function openTab(pageName, element, color){
+    let tabContent, tabButtons;
+    tabContent = document.getElementsByClassName("tabContent");
+    for(let i = 0; i < tabContent.length; i++){
+        tabContent[i].style.display = "none";
+    }
+
+    tabButtons = document.getElementsByClassName("tabButton");
+    for(let i = 0; i < tabButtons.length; i++){
+        tabButtons[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+    element.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
